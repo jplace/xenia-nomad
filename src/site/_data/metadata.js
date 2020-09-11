421a633129f77a9ac8583d0d7fdf2b42a2680f12
Jordan Place <tapman90@gmail.com>
2020-09-11 10:25:25 -0700

    make ogimage

---
+++ src/site/_data/metadata.js
@@ -0,0 +1,5 @@
+module.exports = {
+  url: "https://www.adventurebitch.com", // use this only when absolute URLs are required
+  defaultDescription:
+    "Currently on a roadtrip exploring the beautiful national parks and surrounding nature of the western states.",
+};
