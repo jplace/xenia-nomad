a769d714a74dc22a01cb32d7a11411d3738d6764
Jordan Place <tapman90@gmail.com>
2020-10-19 20:27:50 -0700

    new design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,14 @@ module.exports = {
       fontFamily: {
         display: ["Noto Serif", "serif"],
         body: ["Roboto", "sans-serif"],
+        header: ["Reckless", "sans-serif"],
       },
       colors: {
         finch: "#5A6349",
         "forest-green": "#194d33",
         "pale-rock": "#fef8f6",
+        "soft-black": "#1a1a1a",
+        "light-gray": "#f2f2f2"
       },
     },
   },
