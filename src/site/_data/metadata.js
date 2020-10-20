84a0abc37b0b9b417e9367414ce8dec715e79ace
Jordan Place <tapman90@gmail.com>
2020-10-20 11:20:22 -0700

    change url

--- src/site/_data/metadata.js
+++ src/site/_data/metadata.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: "https://sad-sinoussi-dfd7be.netlify.app/", // use this only when absolute URLs are required
+  url: "https://adventure-bitch.netlify.app/", // use this only when absolute URLs are required
   defaultDescription:
     "Currently on a roadtrip exploring the beautiful national parks and surrounding nature of the western states.",
 };
