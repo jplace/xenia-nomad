2f1c9825d8c46d6f06fb24911370622f0a604f56
Jordan Place <tapman90@gmail.com>
2020-11-19 09:21:11 -0800

    change root domain

--- src/site/_data/metadata.js
+++ src/site/_data/metadata.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: "https://adventure-bitch.netlify.app/", // use this only when absolute URLs are required
+  url: "https://www.adventurebitch.blog", // use this only when absolute URLs are required
   defaultDescription:
     "Currently on a roadtrip exploring the beautiful national parks and surrounding nature of the western states.",
 };
