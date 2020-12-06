f0f5a02dfedb9d647bb1d3fbfb5fcd18805e887e
Jordan Place <tapman90@gmail.com>
2020-12-05 20:45:51 -0800

    ooo it kind of works

---
+++ src/site/_includes/components/Page.js
@@ -0,0 +1,42 @@
+const { html } = require(`htm/preact`);
+
+const Container = require("./Container");
+
+module.exports = ({ children }) => html`
+<div class="min-h-screen flex flex-col">
+  <${Container} moreClasses="home-hero md:border-gray md:border-l md:border-r bg-soft-black overflow-hidden">
+  <div class="home-hero_container">
+    <div class="home-hero_menu">
+      <img src="/images/hero/hero-1.jpg" class="home-hero_menu_img"/>
+      <ul class="home-hero_menu_ul">
+        <li class="inline-block text-lg menu-item">
+          <a class="no-underline" href="/blog">POSTS</a>
+        </li>
+        <li class="inline-block text-lg menu-item">
+          <a class="no-underline" href="/about">ABOUT</a>
+        </li>
+        <li class="inline-block text-lg menu-item">
+          <a class="no-underline" href="https://www.instagram.com/_adventurebitch/"
+            >INSTAGRAM</a>
+        </li>
+      </ul>
+    </div>
+    <img src="/images/hero/hero-2.jpg" class="polaroid home-hero_img2"/>
+    <img src="/images/hero/hero-3.jpg" class="polaroid home-hero_img3"/>
+    <img src="/images/hero/hero-4.jpg" class="polaroid home-hero_img4"/>
+    <h1 class="home-hero_text"><a class="no-underline" href="/">Adventure Bitch</a></h1>  
+  </div>
+  <//>
+  <${Container} moreClasses="flex-grow px-8 pt-6 pb-8 text-black border-gray border-l border-r bg-white">
+    ${children}
+  <//>
+  <${Container} moreClasses="px-4 py-4 text-center border-gray border-l border-r footer-bg">
+  <span class="text-sm text-white"
+    >Made by <a href="https://jplace.github.io/">jplace</a> with
+    <a href="https://www.11ty.dev/">11ty</a>,
+    <a href="https://tailwindcss.com/">Tailwind CSS</a> and
+    <a href="https://www.netlify.com/">Netlify</a>.</span
+  >
+  <//>
+</div>
+`;
--- src/site/_includes/components/page-of.js
+++ src/site/_includes/components/page-of.js
@@ -1,4 +1,5 @@
-<!DOCTYPE html>
+
+module.exports = ({data: {title, metadata }, body, headScripts, headStyles, }) => `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8" />
@@ -6,39 +7,35 @@
     <link rel="icon" type="image/png" href="/images/favicon.png" />
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="{{ title }}" />
+    <meta property="og:title" content="${title}" />
     <meta property="og:site_name" content="Adventure Bitch" />
-    <meta property="og:url" content="{{ metadata.url }}" />
-    <meta property="og:image" content="{{ metadata.url }}/images/ogimage.png" />
+    <meta property="og:url" content="${metadata.url}" />
+    <meta property="og:image" content="${metadata.url}/images/ogimage.png" />
     <meta
       property="og:image:secure_url"
-      content="{{ metadata.url }}/images/ogimage.png"
+      content="${metadata.url}/images/ogimage.png"
     />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
     <meta
       property="og:description"
-      content="{{ metadata.defaultDescription }}"
+      content="${metadata.defaultDescription}"
     />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content="{{ title }}" />
-    <meta name="twitter:site" content="{{ metadata.url }}" />
-    <meta
-      name="twitter:image"
-      content="{{ metadata.url }}/images/ogimage.png"
-    />
+    <meta name="twitter:title" content="${title}" />
+    <meta name="twitter:site" content="${metadata.url}" />
+    <meta name="twitter:image" content="${metadata.url}/images/ogimage.png" />
     <meta
       name="twitter:description"
-      content="{{ metadata.defaultDescription }}"
+      content="${metadata.defaultDescription}"
     />
-    {% block headScripts %}
-    {% endblock %}
-    {% block styles %}
+    ${headScripts || ""}
     <link rel="stylesheet" href="/css/styles.css" />
-    {% endblock %}
-    <title>{{ title }}</title>
+    ${headStyles || ""}
+    <title>${title}</title>
   </head>
   <body class="text-body text-black bg-light-gray">
-    {{ content | safe }}
+    ${body}
   </body>
 </html>
+`;
